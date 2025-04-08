57790a067f06a85b8d71566e12a0414e4bad01c8
Luke Sullivan <sully097@gmail.com>
2025-04-08 16:23:37 +0100

    add html skeleton

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const library = [];
 
-function Book() {
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
 
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = {
+        book: new Book(title, author, pages, read),
+        id: crypto.randomUUID()
+    }
+
+    library.push(newBook);
 }
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+function displayBooks() {
+    for (const book of library) {
+        
+    }
+}
+
+console.log(library[0]);
