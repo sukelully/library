2f697d58293a047dfa07c376ec5121d49ab0c54e
Luke Sullivan <sully097@gmail.com>
2025-04-08 16:42:01 +0100

    Get display semi-working:

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tbody = document.getElementById('table-body');
+
 const library = [];
 
 function Book(title, author, pages, read) {
@@ -21,11 +23,41 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBookToLibrary('The Martian', 'Andy Weir', 300, true);
+addBookToLibrary('The Queen\'s Gambit', 'Walter Tevis', 400, true);
+addBookToLibrary('The Road', 'Cormac McCarthy', 500, false);
 
 function displayBooks() {
     for (const book of library) {
-        
+        console.log(book);
     }
 }
 
-console.log(library[0]);
+function addTableRow() {
+    const newRow = document.createElement('tr');
+
+    for (const bookObj of library) {
+        const book = bookObj.book;
+
+        const titleCell = document.createElement('td');
+        titleCell.textContent = book.title;
+
+        const authorCell = document.createElement('td');
+        authorCell.textContent = book.author;
+
+        const pagesCell = document.createElement('td');
+        pagesCell.textContent = book.pages;
+
+        const readCell = document.createElement('td');
+        readCell.textContent = book.read ? 'Yes' : 'No';
+
+        newRow.appendChild(titleCell);
+        newRow.appendChild(authorCell);
+        newRow.appendChild(pagesCell);
+        newRow.appendChild(readCell);
+    }
+
+    tbody.appendChild(newRow);
+}
+
+addTableRow();
